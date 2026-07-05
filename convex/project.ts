60405d16eb8cd82bd13a5982510d0eb78cce1bb4
ayush mandal <184559696+Ayush-Rusty@users.noreply.github.com>
2026-07-05 21:25:41 +0530

    added project actions related stuffs

---
+++ convex/project.ts
@@ -0,0 +1,30 @@
+import {v} from "convex/values";
+import {mutation, query} from "./_generated/server"
+
+export const create=mutation({
+    args:{
+        name:v.string()
+    },
+    handler:async(ctx,args)=>{
+        const identity=await ctx.auth.getUserIdentity();
+        if(!identity){
+            throw new Error("unauthorized user access")
+        }
+
+        await ctx.db.insert("projects",{
+            name:args.name,
+            ownerId:identity?.subject
+        })
+    }
+})
+
+export const get=query({
+    args:{},
+    handler:async(ctx)=>{
+        const identity=await ctx.auth.getUserIdentity();
+        if(!identity){
+            throw new Error("Unauthorized user access")
+        }
+        return await ctx.db.query("projects").collect();
+    }
+})
