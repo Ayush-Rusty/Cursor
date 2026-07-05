46a6b026a727a7779626bf469b88bc756e6e240f
ayush mandal <184559696+Ayush-Rusty@users.noreply.github.com>
2026-07-05 17:04:34 +0530

    initailized the repo

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { ModeToggle } from "@/components/mode-toggle";
+import {Button} from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <div className="text-blue font-bold ">
+      <Button>
+      something went wrong
+      </Button>
+      <ModeToggle/>
+    </div>
+  );
+}
