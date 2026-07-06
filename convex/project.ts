8453544c225d2ef83d453c7f0e58235b630de74d
ayush mandal <184559696+Ayush-Rusty@users.noreply.github.com>
2026-07-06 18:22:07 +0530

    added get Partial functionality

--- convex/project.ts
+++ convex/project.ts
@@ -1,30 +1,33 @@
 import {v} from "convex/values";
 import {mutation, query} from "./_generated/server"
+import { verifyAuth } from "./auth";
+
 
 export const create=mutation({
     args:{
         name:v.string()
     },
     handler:async(ctx,args)=>{
-        const identity=await ctx.auth.getUserIdentity();
-        if(!identity){
-            throw new Error("unauthorized user access")
-        }
+        const identity=await verifyAuth(ctx)
 
-        await ctx.db.insert("projects",{
+        const ProjectId=await ctx.db.insert("projects",{
             name:args.name,
-            ownerId:identity?.subject
+            ownerId:identity.subject,
+            updatedAt:Date.now(),
         })
+
+        return ProjectId
     }
 })
 
-export const get=query({
-    args:{},
-    handler:async(ctx)=>{
-        const identity=await ctx.auth.getUserIdentity();
-        if(!identity){
-            throw new Error("Unauthorized user access")
-        }
-        return await ctx.db.query("projects").collect();
+export const getPartial=query({
+    args:{
+        limit:v.number(),
+    },
+    handler:async(ctx,args)=>{
+        const identity=await verifyAuth(ctx)
+        return await ctx.db.query("projects").withIndex("by_owner",(q)=>q.eq("ownerId",identity.subject)).take(args.limit);
     }
 })
+
+
