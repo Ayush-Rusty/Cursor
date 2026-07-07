0d2849d41cd2df07c6583457e7013d9414ce8903
ayush mandal <184559696+Ayush-Rusty@users.noreply.github.com>
2026-07-07 17:42:27 +0530

    added some functionality

--- convex/project.ts
+++ convex/project.ts
@@ -30,11 +30,32 @@ export const get=query({
         return await ctx.db.query("projects").withIndex("by_owner",(q)=>q.eq("ownerId",identity.subject)).take(args.limit);
     }
 })
+
 export const getPartial=query({
     handler:async(ctx)=>{
 
         const identity=await verifyAuth(ctx)
         return await ctx.db.query("projects").withIndex("by_owner",(q)=>q.eq("ownerId",identity.subject)).collect();
+    }
+})
+
+
+export const getById=query({
+    args:{
+        id:v.id("projects")
+    },
+    handler:async(ctx,args)=>{
+        const identity=await verifyAuth(ctx)
+        const project=await ctx.db.get("projects",args.id)
+
+       if(!project){
+        throw new Error("Project not found")
+       }
+
+       if(project.ownerId!==identity.subject){
+        throw new Error("Unauthorized access to this project")
+       }
+       return project;
     }
 })
 
