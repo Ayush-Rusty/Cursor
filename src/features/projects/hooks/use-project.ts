a5d15e1e1cc6411d3590fa9e21375b19b8a5e61b
ayush mandal <184559696+Ayush-Rusty@users.noreply.github.com>
2026-07-07 14:53:48 +0530

    added some createProject functionality

--- src/features/projects/hooks/use-project.ts
+++ src/features/projects/hooks/use-project.ts
@@ -1,5 +1,8 @@
+/* eslint-disable react-hooks/purity */
 import { useMutation, useQuery } from "convex/react";
 import {api} from "../../../../convex/_generated/api"
+import { Id } from "convex/_generated/dataModel";
+
 
 export const UseProjects=()=>{
     return useQuery(api.project.get,{limit:10})
@@ -10,5 +13,24 @@ export const UseProjectsPartial=(limit:number)=>{
 }
 
 export const useCreateProject=()=>{
-    return useMutation(api.project.create);
+    
+    return useMutation(api.project.create).withOptimisticUpdate(
+        (localStore,args)=>{
+            const existingProjects=localStore.getQuery(api.project.get,{limit:10})
+            if(existingProjects!==undefined){
+                const now=Date.now();
+                const newProject={
+                    _id:crypto.randomUUID() as Id<"projects">,
+                    _creationTime:now,
+                    name:args.name,
+                    ownerId:"anonymous",
+                    updatedAt:now
+                 };
+                 localStore.setQuery(api.project.get,{limit:10},[
+                    newProject,
+                    ...existingProjects,
+                 ])
+            }
+        }
+    )
 }
